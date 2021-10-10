233450468eb7f020b524d5c9bf8a4c8a36c6cd4c
Maurice Verheijen <maurice8nz@gmail.com>
2021-10-10 23:26:24 +1300

    Handling errors and time span > 24h; pm times auto

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,38 +21,52 @@ namespace TimeCalc
 	/// </summary>
 	public partial class MainWindow : Window
 	{
-		public const string TimeFormat = "h':'mm";
+		private static readonly Regex RangeRx = new Regex(@"^(?<start>\d\d?:\d\d)[- /]+(?<end>\d\d?:\d\d\b)", RegexOptions.Compiled);
+		private static readonly Regex TargetRx = new Regex(@"^(?<hours>\d+)(?::(?<minutes>\d\d))?$", RegexOptions.Compiled);
+
 		public MainWindow()
 		{
 			InitializeComponent();
 			Times.Text = Settings.Default.Times;
-			Target.Text = Settings.Default.Target.ToString(TimeFormat);
+			Target.Text = Settings.Default.Target;
 		}
 
 		private void Total_Click(object sender, RoutedEventArgs e)
 		{
-			try
-			{
+			try {
 				string[] times = Times.Text.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
-				TimeSpan sum = TimeSpan.Zero;
-				foreach (var range in times)
-				{
-					string s = range.Trim();
-					int p = s.IndexOf(' ');
-					TimeSpan from, to;
-					if (p != -1 && TimeSpan.TryParse(s.Substring(0, p), out from) && TimeSpan.TryParse(s.Substring(p + 1), out to))
-					{
-						sum += to - from;
+				var sum = TimeSpan.Zero;
+				var sb = new StringBuilder();
+				Match match;
+				foreach (var range in times) {
+					match = RangeRx.Match(range);
+					if (match.Success) {
+						sb.AppendLine(range);
+						string start = match.Groups["start"].Value;
+						string end = match.Groups["end"].Value;
+						if (TimeSpan.TryParse(start, out var from) && TimeSpan.TryParse(end, out var to)) {
+							if (from > to) to += TimeSpan.FromHours(12);
+							sum += to - from;
+						}
+					}
+					else {
+						if (!range.EndsWith(" <error")) sb.AppendLine(range + " <error");
+						else sb.AppendLine(range);
 					}
 				}
-				TotalSoFar.Content = sum.ToString(TimeFormat);
-				TimeSpan target;
-				if (TimeSpan.TryParse(Target.Text, out target))
-				{
-					var content = (target - sum).ToString(TimeFormat);
+				TotalSoFar.Content = sum.ToString("c");
+				match = TargetRx.Match(Target.Text);
+				if (match.Success) {
+					int hours = int.TryParse(match.Groups["hours"].Value, out int h) ? h : 0;
+					int minutes = int.TryParse(match.Groups["minutes"].Value, out int m) ? m : 0;
+					var target = new TimeSpan(hours, minutes, 0);
+					var content = (target - sum).ToString("c");
 					if (sum > target) content += " over";
 					TTG.Content = content;
+				} else {
+					TTG.Content = "error";
 				}
+				Times.Text = sb.ToString();
 			}
 			catch { }
 		}
@@ -70,11 +85,7 @@ namespace TimeCalc
 		private void Save()
 		{
 			Settings.Default.Times = Times.Text;
-			TimeSpan ts;
-			if (TimeSpan.TryParse(Target.Text, out ts))
-			{
-				Settings.Default.Target = ts;
-			}
+			Settings.Default.Target = Target.Text;
 			Settings.Default.Save();
 		}
 	}
